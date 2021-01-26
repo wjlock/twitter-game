482828efbef9dc76d07f46d75a3aee99a49eafd4
Will <will.locklier@gmail.com>
2021-01-26 15:20:20 -0500

    Add generate tweet functionality

--- src/components/GameRounds.js
+++ src/components/GameRounds.js
@@ -1,34 +1,61 @@
 import React, { useEffect, useState } from 'react';
 import axios from "axios";
+import useSWR from "swr"
 // var Twitter = require('twitter');
 // var client = new Twitter({
 //     consumer_key: process.env.TWITTER_CONSUMER_KEY,
 //     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
 //     bearer_token: process.env.TWITTER_BEARER_TOKEN
 //   });
-  
+const fetcher = (...args) => fetch(...args).then((res) => res.json())
 
 
 const GameRounds = () => {
     // Set State
-    const [answers, setAnswers] = useState(["Answer 1", "Answer 2", "Answer 3", "Answer 4"])
-    const [randomTweet, setRandomTweet] = useState('This is a random tweet')
+    const [answers, setAnswers] = useState([])
+    const [randomTweet, setRandomTweet] = useState('')
     const [gameRound, setGameRound] = useState(1)
     const [points, setPoints] = useState(0)
     const [correctAnswer, setCorrectAnswer] = useState('Answer 1')
     const [userAnswer, setUserAnswer] = useState('')
+    const { data, error } = useSWR("http://localhost:8000/api/calls/newtweet", fetcher)
+    console.log(data)
     const handleUserAnswer = (e) => {
         setUserAnswer(e.target.value)
         console.log(userAnswer)
     }
+    function getRndInteger(min, max) {
+        return Math.floor(Math.random() * (max - min) ) + min;
+    }
     const createAnswers = answers.map((answer) => {
         return <button value={answer} onClick={handleUserAnswer}>{answer}</button>
-    }) 
+    })
+    function generateTweet() {
+        const randomNumber = getRndInteger(0, 10)
+        const tweet = data.statuses[randomNumber].full_text
+        setRandomTweet(tweet)
+        setCorrectAnswer(data.statuses[randomNumber].user.name)
+        answers.push(correctAnswer)
+        console.log(answers)
+        console.log(correctAnswer)
+    }
+
+    // useEffect(() => {
+    //    const firstTweet = data.statuses[getRndInteger(0, 10)].full_text
+    //    setRandomTweet(firstTweet)
+    // })
+
+    // useEffect(async () => {
+    //     const newTweets = await axios.get("http://localhost:8000/api/calls/newtweet")
+    //     // .then((newTweets) => console.log(newTweets.data.statuses[2].text))
+    //     // setRandomTweet(newTweets.data.statuses[5].text)
+    //     // setRandomTweet(newTweets.data.statuses[5].text)
+    //     setTweetData(newTweets)
+    //     console.log(tweetData)
+    // }, [])
 
     const handleSubmit = async (e) => {
-        const setNewTweet = await axios
-            .get("http://localhost:8000/api/calls/newtweet")
-            .then(console.log(setNewTweet))
+        e.preventDefault()
         //check if they have the right answer
         //add pints if the answer is correct
         // increase the round number (everytime)
@@ -36,18 +63,32 @@ const GameRounds = () => {
         // set the new correct answer
         // set the remainning random answers
         // check id round number is 10, if so end the game
-        if (userAnswer === correctAnswer) {
+        if (userAnswer === correctAnswer && gameRound === 10) {
+            setPoints(points +100)
+            alert("Game Over")
+        } else if (userAnswer != correctAnswer && gameRound === 10){
+            // end the game
+            // display summary
+
+        } else if (userAnswer === correctAnswer && gameRound < 10) {
             setPoints(points +100)
             setGameRound(gameRound +1)
-            console.log(setNewTweet)
-            
-        } 
+            generateTweet()
+        } else if (userAnswer != correctAnswer && gameRound < 10) {
+            setGameRound(gameRound +1)
+            generateTweet()
+            // display new tweet
+            // display new answers
+            // display notification the answer was incorrect
+        } else {
+            alert('Please select an answer before clicking submit')
+        }
     }
-
     return (
         
 
         <div>
+            <button onClick={generateTweet}>Generate the first tweet</button>
             <h1>Points: {points}</h1>
             <h1>Round {gameRound}</h1>
             <h2>{randomTweet}</h2>  
